743faf13e93723392d4bdd2fba1a1877e3714f3b
yasuuuuu <yasu_figure8@yahoo.co.jp>
2017-08-17 23:39:53 +0900

    section4

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,30 +1,80 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
     return (
-      <div>
+      <div className={className}>
+        <label>{field.label}</label>
         <input
+          className="form-control"
           type="text"
           {...field.input}
         />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
 
+  renderTagsField() {
+
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
+          label="Title For Post"
           name="title"
-          component={this.renderTitleField}
+          component={this.renderField}
         />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a title';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter a categories!';
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter a content please!';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
